0ea93e1ec1ce07ceab062fce113c2ac4d1fb747f
pfe1223 <patrick.ester@gmail.com>
2018-05-16 10:23:37 -0700

    Class Work
    Made updates to code as I followed along with the instructor.

--- web-audio-workshop-master/part1/1-oscillator/sketch.js
+++ web-audio-workshop-master/part1/1-oscillator/sketch.js
@@ -1,27 +1,27 @@
-//set AudioContext class for compatibility 
-let AudioContext = window.AudioContext || window.webkitAudioContext;  
+//set AudioContext class for compatibility
+let AudioContext = window.AudioContext || window.webkitAudioContext;
 
 //create audio context
 const audioContext = new AudioContext();
 
 //setup oscillator
 const oscillator = audioContext.createOscillator();
-oscillator.type = "square";
-oscillator.frequency.value = "440";
-oscillator.detune.value = "-2400";//cents
+oscillator.type = "sine";
+oscillator.frequency.value = 440;
+oscillator.detune.value = -2400; //cents
 oscillator.start();
-oscillator.connect( audioContext.destination );
+oscillator.connect(audioContext.destination);
 
 function setup() {
 
-	//resume web audio on first click for Chrome autoplay rules
-	function clickHandler(){
-		audioContext.resume();
-		document.body.removeEventListener( "click", clickHandler );
-	}
-	document.body.addEventListener( "click", clickHandler );
+  //resume web audio on first click for Chrome autoplay rules
+  function clickHandler() {
+    audioContext.resume();
+    document.body.removeEventListener("click", clickHandler);
+  }
+  document.body.addEventListener("click", clickHandler);
 
-	//create p5 canvas
-	createCanvas( windowWidth, windowHeight );
+  //create p5 canvas
+  createCanvas(windowWidth, windowHeight);
 
 }
