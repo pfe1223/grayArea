cc68c93fa1a0a9ba755c09d952daa1323dcf3e5e
pfe1223 <patrick.ester@gmail.com>
2018-05-18 07:48:32 -0700

    Class Work
    Updating class examples to learn how to use web audio.

--- web-audio-workshop-master/part2/8-samples/sketch.js
+++ web-audio-workshop-master/part2/8-samples/sketch.js
@@ -1,12 +1,12 @@
-//set AudioContext class for compatibility 
-let AudioContext = window.AudioContext || window.webkitAudioContext;  
+//set AudioContext class for compatibility
+let AudioContext = window.AudioContext || window.webkitAudioContext;
 
 //create audio context
 const audioContext = new AudioContext();
 
 //setup master gain
 const masterGain = audioContext.createGain();
-masterGain.connect( audioContext.destination );
+masterGain.connect(audioContext.destination);
 masterGain.gain.value = .8;
 
 //setup bus and effects
@@ -20,27 +20,39 @@ const compressor = audioContext.createDynamicsCompressor();
 compressor.connect(masterGain);
 
 const submixGain = audioContext.createGain();
-submixGain.connect( compressor );
+submixGain.connect(compressor);
 
 const effectGain = audioContext.createGain();
-effectGain.connect( compressor );
+effectGain.connect(compressor);
 
-const delay = new Delay( { audioContext, feedback: .4, time: .5 } );
-submixGain.connect( delay.input );
-delay.output.connect( effectGain );
+const delay = new Delay({
+  audioContext,
+  feedback: .1,
+  time: .1
+});
+submixGain.connect(delay.input);
+delay.output.connect(effectGain);
 
-const reverb = new Reverb( { audioContext, url: "/audio/impulses/default.wav" } );
-submixGain.connect( reverb.input );
-reverb.output.connect( effectGain );
+const reverb = new Reverb({
+  audioContext,
+  url: "/audio/impulses/default.wav"
+});
+submixGain.connect(reverb.input);
+reverb.output.connect(effectGain);
 
-delay.output.connect( reverb.input );
+delay.output.connect(reverb.input);
 
 //setup analyser
-const analyser = new Analyser( { audioContext } );
-masterGain.connect( analyser.input );
+const analyser = new Analyser({
+  audioContext
+});
+masterGain.connect(analyser.input);
 
 //setup musical scale and keyboard
-const musicalScale = new MusicalScale({ scale: "minor", rootNote: "A4" });
+const musicalScale = new MusicalScale({
+  scale: "major",
+  rootNote: "A4"
+});
 const keyboardKeyCount = 14;
 const slideTime = .5;
 let currentKeyboardKey = 0;
@@ -52,51 +64,54 @@ let voice;
 
 function setup() {
 
-	//resume web audio on first click for Chrome autoplay rules
-	function clickHandler(){
-		audioContext.resume();
-		document.body.removeEventListener( "click", clickHandler );
-	}
-	document.body.addEventListener( "click", clickHandler );
+  //resume web audio on first click for Chrome autoplay rules
+  function clickHandler() {
+    audioContext.resume();
+    document.body.removeEventListener("click", clickHandler);
+  }
+  document.body.addEventListener("click", clickHandler);
 
-	//create p5 canvas
-	createCanvas( windowWidth, windowHeight );
+  //create p5 canvas
+  createCanvas(windowWidth, windowHeight);
 
-	polyVoice = new PolyVoice( { audioContext, VoiceClass: Voice } );
-	polyVoice.output.connect( submixGain );
-	voice = polyVoice.currentVoice;
+  polyVoice = new PolyVoice({
+    audioContext,
+    VoiceClass: Voice
+  });
+  polyVoice.output.connect(submixGain);
+  voice = polyVoice.currentVoice;
 
-	let sampleURLs = ["/audio/samples/wolves.mp3"];
+  let sampleURLs = ["/audio/samples/paperTiger.mp3"];
 
-	AudioBufferLoader.load( sampleURLs, audioContext )
-	  .then( buffers => {
-	    polyVoice.voiceMap.forEach( voice => {
-	      voice.buffers = buffers;
-	    })
-	  });
+  AudioBufferLoader.load(sampleURLs, audioContext)
+    .then(buffers => {
+      polyVoice.voiceMap.forEach(voice => {
+        voice.buffers = buffers;
+      })
+    });
 
 
 }
 
-function mousePressed(){
+function mousePressed() {
 
-	polyVoice.start();
-	voice = polyVoice.currentVoice;
+  polyVoice.start();
+  voice = polyVoice.currentVoice;
 
-	updateKeyboardKey();
+  updateKeyboardKey();
 
 }
 
 function mouseReleased() {
 
-	voice.stop();
-	voice = polyVoice.currentVoice;
+  voice.stop();
+  voice = polyVoice.currentVoice;
 
 }
 
 function mouseDragged() {
 
-	updateKeyboardKeySlide();
+  updateKeyboardKeySlide();
 
 }
 
@@ -105,64 +120,64 @@ function mouseMoved() {
 }
 
 function updateKeyboardKey() {
-	
-	let k = Math.floor( ( mouseX / windowWidth ) * keyboardKeyCount );
 
-	currentKeyboardKey = k;
-	voice.oscillator.frequency.cancelScheduledValues( audioContext.currentTime );
-	voice.oscillator.frequency.setValueAtTime( musicalScale.getFrequency( currentKeyboardKey ), audioContext.currentTime );
+  let k = Math.floor((mouseX / windowWidth) * keyboardKeyCount);
+
+  currentKeyboardKey = k;
+  voice.oscillator.frequency.cancelScheduledValues(audioContext.currentTime);
+  voice.oscillator.frequency.setValueAtTime(musicalScale.getFrequency(currentKeyboardKey), audioContext.currentTime);
 
 }
 
 function updateKeyboardKeySlide() {
-	
-	let k = Math.floor( ( mouseX / windowWidth ) * keyboardKeyCount );
 
-	if( k !== currentKeyboardKey ) {
-		currentKeyboardKey = k;
-		voice.oscillator.frequency.cancelScheduledValues( audioContext.currentTime );
-		voice.oscillator.frequency.linearRampToValueAtTime( musicalScale.getFrequency( currentKeyboardKey ), audioContext.currentTime + slideTime );
-	}
+  let k = Math.floor((mouseX / windowWidth) * keyboardKeyCount);
+
+  if (k !== currentKeyboardKey) {
+    currentKeyboardKey = k;
+    voice.oscillator.frequency.cancelScheduledValues(audioContext.currentTime);
+    voice.oscillator.frequency.linearRampToValueAtTime(musicalScale.getFrequency(currentKeyboardKey), audioContext.currentTime + slideTime);
+  }
 
 }
 
 function draw() {
 
-	//clear canvas
-  stroke( 200 );
-	fill( 255, 255, 255 );
+  //clear canvas
+  stroke(200);
+  fill(255, 255, 255);
 
-	//draw keyboard
-	let keyboardKeyWidth = windowWidth / keyboardKeyCount;
-	for( var i = 0; i < keyboardKeyCount; i++ ){
-		rect( i * keyboardKeyWidth, 0, keyboardKeyWidth, windowHeight );
-	}
+  //draw keyboard
+  let keyboardKeyWidth = windowWidth / keyboardKeyCount;
+  for (var i = 0; i < keyboardKeyCount; i++) {
+    rect(i * keyboardKeyWidth, 0, keyboardKeyWidth, windowHeight);
+  }
 
-	//draw waveform
-  stroke( 255, 0, 0 );
-	strokeWeight( 4 );
+  //draw waveform
+  stroke(255, 0, 0);
+  strokeWeight(4);
 
-	let dataArray = analyser.getWaveformData();
-	let sliceWidth = windowWidth / dataArray.length;
-	let waveformHeight = .333 * windowHeight;
+  let dataArray = analyser.getWaveformData();
+  let sliceWidth = windowWidth / dataArray.length;
+  let waveformHeight = .333 * windowHeight;
 
-	for( var i = 1; i < dataArray.length; i++ ) {
-		
-		//start point of line segment
-		let x1 = ( i - 1 ) * sliceWidth;//time
-		let y1 = waveformHeight * ( dataArray[ i - 1 ] / 256 );//amplitude
+  for (var i = 1; i < dataArray.length; i++) {
 
-		//end point of line segment
-		let x2 = i * sliceWidth;//time
-		let y2 = waveformHeight * ( dataArray[ i ] / 256 );//amplitude
+    //start point of line segment
+    let x1 = (i - 1) * sliceWidth; //time
+    let y1 = waveformHeight * (dataArray[i - 1] / 256); //amplitude
 
-		//offset Y to middle of window
-		y1 += ( .5 * windowHeight ) - ( .5 * waveformHeight );
-		y2 += ( .5 * windowHeight ) - ( .5 * waveformHeight );
+    //end point of line segment
+    let x2 = i * sliceWidth; //time
+    let y2 = waveformHeight * (dataArray[i] / 256); //amplitude
 
-		//draw line segment
-		line( x1, y1, x2, y2 );
+    //offset Y to middle of window
+    y1 += (.5 * windowHeight) - (.5 * waveformHeight);
+    y2 += (.5 * windowHeight) - (.5 * waveformHeight);
 
-	}
+    //draw line segment
+    line(x1, y1, x2, y2);
+
+  }
 
 }
