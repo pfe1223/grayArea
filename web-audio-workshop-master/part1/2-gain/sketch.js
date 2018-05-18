a0cd161e4d1da8134e2a0c4239d51bc5377a4d0f
pfe1223 <patrick.ester@gmail.com>
2018-05-17 18:04:10 -0700

    Updated Web Audio
    Class work for the second day of web audio.

--- web-audio-workshop-master/part1/2-gain/sketch.js
+++ web-audio-workshop-master/part1/2-gain/sketch.js
@@ -1,55 +1,50 @@
-//set AudioContext class for compatibility
-let AudioContext = window.AudioContext || window.webkitAudioContext;
+//set AudioContext class for compatibility 
+let AudioContext = window.AudioContext || window.webkitAudioContext;  
 
 //create audio context
 const audioContext = new AudioContext();
 
 //setup master gain
 const masterGain = audioContext.createGain();
-masterGain.connect(audioContext.destination);
+masterGain.connect( audioContext.destination );
 masterGain.gain.value = 0;
 
 //setup oscillator
 const oscillator = audioContext.createOscillator();
 oscillator.start();
-oscillator.connect(masterGain);
+oscillator.connect( masterGain );
 
 function setup() {
 
-  //resume web audio on first click for Chrome autoplay rules
-  function clickHandler() {
-    audioContext.resume();
-    document.body.removeEventListener("click", clickHandler);
-  }
-  document.body.addEventListener("click", clickHandler);
+	//resume web audio on first click for Chrome autoplay rules
+	function clickHandler(){
+		audioContext.resume();
+		document.body.removeEventListener( "click", clickHandler );
+	}
+	document.body.addEventListener( "click", clickHandler );
 
-  //listen for oscillator waveform selection
-  const oscWaveformElement = document.querySelector("#osc-waveform");
-  oscWaveformElement.addEventListener("change", function(event) {
-    event.preventDefault();
-    oscillator.type = event.target.value;
-  });
+	//listen for oscillator waveform selection
+	const oscWaveformElement = document.querySelector( "#osc-waveform" );
+	oscWaveformElement.addEventListener( "change", function( event ){
+		event.preventDefault();
+		oscillator.type = event.target.value;
+	});
 
-  //create p5 canvas
-  createCanvas(windowWidth, windowHeight);
+	//create p5 canvas
+	createCanvas( windowWidth, windowHeight );
 
 }
 
-function mousePressed() {
-  //oscillator.type = "sine";
-  oscillator.frequency.value = 440;
-  oscillator.detune.value = -2400; //cents
-  masterGain.gain.setValueAtTime(1, audioContext.currentTime);
+function mousePressed(){
+
 }
 
 function mouseReleased() {
-  masterGain.gain.setValueAtTime(0, audioContext.currentTime);
+
 }
 
 function mouseDragged() {
-  let val = map(mouseX, 0, width, -1760, 440, true);
-  oscillator.detune.value = val; //cents
-  masterGain.gain.setValueAtTime(1, audioContext.currentTime);
+
 }
 
 function mouseMoved() {
@@ -58,8 +53,8 @@ function mouseMoved() {
 
 function draw() {
 
-  //clear canvas
-  stroke(200);
-  fill(255, 255, 255);
+	//clear canvas
+  stroke( 200 );
+	fill( 255, 255, 255 );
 
 }
