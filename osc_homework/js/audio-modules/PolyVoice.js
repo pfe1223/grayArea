b5e15c65c273ec1da6fa572c714b6d5336c7c86c
pfe1223 <patrick.ester@gmail.com>
2018-05-19 13:13:04 -0700

    Add Web Audio
    Adding web audio stuff from Ray to the project.

---
+++ osc_homework/js/audio-modules/PolyVoice.js
@@ -0,0 +1,56 @@
+class PolyVoice {
+  
+  constructor( options ) {
+
+    this.audioContext = options.audioContext;
+
+    //class to make a polyphonic version of. Requires an output and start/stop methods
+    let VoiceClass = options.VoiceClass;
+
+    //how many voices to create
+    this.voiceCount = options.voiceCount || 6;
+
+    this.currentVoiceId = 0;
+    
+    this.voiceMap = new Map();
+
+    this.output = this.audioContext.createGain();
+    this.output.gain.value = 1;
+
+    //create the voices
+    for ( let i=0; i<this.voiceCount; i++ ) {
+
+      let voice = new VoiceClass( { audioContext: this.audioContext } );
+      voice.output.connect( this.output );
+
+      this.voiceMap.set( i, voice );
+
+    }
+
+    this.currentVoice = this.voiceMap.get(0);
+
+  }
+
+  start( time = this.audioContext.currentTime, voiceId ){
+
+    //cycle through the voices
+    this.currentVoiceId = voiceId || ( this.currentVoiceId + 1 ) % this.voiceCount;
+    
+    this.currentVoice = this.voiceMap.get( this.currentVoiceId );
+
+    this.currentVoice.start( time );
+
+    return this.currentVoice;
+
+  }
+
+  stop( time = this.audioContext.currentTime, voiceId ){
+
+    let vId = voiceId || this.currentVoice
+
+    voiceMap.get( vId ).stop( time );
+
+  }
+
+
+}
