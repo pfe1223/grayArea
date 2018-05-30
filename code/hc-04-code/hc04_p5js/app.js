3791c622293108241d875379cbf79670282c1531
pfe1223 <patrick.ester@gmail.com>
2018-05-30 10:56:13 -0700

    Class Work
    Code changes to examples provided by the instructors.

--- code/hc-04-code/hc04_p5js/app.js
+++ code/hc-04-code/hc04_p5js/app.js
@@ -5,22 +5,24 @@ var path = require("path");
 const io = require("socket.io")(server);
 const SerialPort = require("serialport");
 const Readline = SerialPort.parsers.Readline;
-const port = new SerialPort("/dev/tty.usbmodem143741", {
-    baudRate: 9600
+const port = new SerialPort("/dev/tty.usbmodem1411", {
+  baudRate: 9600
 });
 
 //expose the local public folder for inluding files js, css etc..
 app.use(express.static("public"));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
-const parser = port.pipe(new Readline({ delimiter: "\r\n" }));
+const parser = port.pipe(new Readline({
+  delimiter: "\r\n"
+}));
 
 parser.on("data", function(data) {
-    io.sockets.emit('data', {
-        val: data
-    });
-    console.log(data);
+  io.sockets.emit('data', {
+    val: data
+  });
+  console.log(data);
 });
